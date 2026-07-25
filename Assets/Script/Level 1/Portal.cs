735112e9283e297e005b507de907d43aa49e1aa4
Aaditya <aadisingh3217@gmail.com>
2026-07-26 00:01:35 +0530

    Changes

--- Assets/Script/Level 1/Portal.cs
+++ Assets/Script/Level 1/Portal.cs
@@ -3,8 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    private const int FinalLevelBuildIndex = 3;
-
     [SerializeField] private GameObject winCanvas;
 
     void OnTriggerEnter2D(Collider2D other)
@@ -13,23 +11,6 @@ public class Portal : MonoBehaviour
         {
             return;
         }
-
-        int activeBuildIndex = SceneManager.GetActiveScene().buildIndex;
-
-        if (activeBuildIndex == FinalLevelBuildIndex)
-        {
-            if (winCanvas != null)
-            {
-                winCanvas.SetActive(true);
-            }
-
-            SceneManager.LoadScene(activeBuildIndex + 1);
-            return;
-        }
-
-        if (activeBuildIndex < FinalLevelBuildIndex)
-        {
-            SceneManager.LoadScene(activeBuildIndex + 1);
-        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
