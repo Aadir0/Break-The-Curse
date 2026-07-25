52d5638a844e605ce56c86bf55a32040070fb969
Aaditya <aadisingh3217@gmail.com>
2026-07-26 00:42:28 +0530

    Changes

--- Assets/Script/UI/NewDisplay.cs
+++ Assets/Script/UI/NewDisplay.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class NewDisplay : MonoBehaviour
 {
     [SerializeField] private TutorialWriting CanvasConttroller;
+    [SerializeField] private GameObject manager;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            manager.SetActive(true);
             if (CanvasConttroller == null)
             {
                 CanvasConttroller = FindAnyObjectByType<TutorialWriting>();
